dab2b96a814974e0dd4d5a74c4bc908a8322e82c
AirDur <ralphagris@gmail.com>
2019-05-02 14:30:25 +0200

    Exo 7

--- Rendus TP1/TP1 - Romain Duret.r
+++ Rendus TP1/TP1 - Romain Duret.r
@@ -32,13 +32,13 @@ coeff_spearman <- function(X, Y) {
   somme = 0
   N = length(X)
   R_x = rank(X)$
-  R_y = rank(Y)
+    R_y = rank(Y)
   for (i in seq(1, N)) {
     provi = R_x[i] - R_y[i]
     a_add = provi^2
     somme = somme + a_add
   }
-    
+  
   result = 1 - ((6*somme)/(N^3 - N))
   result
 }
@@ -69,7 +69,7 @@ if(test_independance_var_quant(my_data2$Marseille, 19)<=2.145) {
 } else {
   print("H1 : hyothese invalide, l'inflation a affecté le cout ")
 }
-  
+
 test_independance_deux_var_quant = function(X, Y) {
   m1 = mean(X)
   m2 = mean(Y)
@@ -95,6 +95,10 @@ if(test_independance_deux_var_quant(my_data2$Marseille, my_data2$Aix)<=2.468) {
   print("H1 : hyothese invalide, il existe une dependance significatif entre Aix et Marseille ")
 }
 
+####### EXERCICE 7 : 
+
+# b) Fonction du khi deux.
+# k, O, E
 fonction_khi_deux = function(nombre_classe_distinct, valeur_observee, valeur_theorique) {
   somme = 0
   for (i in seq(1, nombre_classe_distinct)) {
@@ -105,3 +109,22 @@ fonction_khi_deux = function(nombre_classe_distinct, valeur_observee, valeur_the
   }
   somme
 }
+
+# valeurs théoriques : 859.5 / 19.875 / 21.9375 / 0.0625
+nombre_phenotype = 4 # k
+ratio_generiques = c(9, 3, 3, 1) # ratio génériques pour la présence de chaque phénotype
+valeur_observee = c(1528, 106, 117, 381) # valeurs observees pour chaque phénotype de plante
+nombre_plante_observee = sum(valeur_observee) # n, nombre de plante observee au total
+valeur_theorique = c(0, 0, 0, 0)
+# a)  calcul des valeurs theoriques : (nombre de plante * (ratio_du_phenotype/somme_des_ratios))
+for(i in seq(1, nombre_phenotype)) {
+  valeur_theorique[i] = nombre_plante_observee * (ratio_generiques[i]/sum(ratio_generiques))
+}
+# 
+print(fonction_khi_deux(nombre_phenotype, valeur_observee, valeur_theorique))
+# 966.61
+
+# c) valeur théorique : 7.81
+# différence vraiment grande = aucune corrélation
+
+# Exercice 8 : 
